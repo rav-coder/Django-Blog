9e41fb075d99a143b7e6ee4d783b62a6352414f2
Saurav <76596911+rav-coder@users.noreply.github.com>
2023-03-17 21:30:17 -0600

    admin page for emotion and updated auth_user

--- reflecty/diary/admin.py
+++ reflecty/diary/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Emotion
 
 # Register your models here.
+class EmotionAdmin(admin.ModelAdmin):
+    list_display = ['subtype', 'type']
+    list_filter = ['type']
+
+admin.site.register(Emotion, EmotionAdmin)
